c47d6508fab1a48f4537ab0258f3ae7a3a015558
Ivan Mitenkov <39021235+MitenkovIvan@users.noreply.github.com>
2020-04-29 13:40:42 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const Eris = require("eris");
 const client = new Eris(require("./config.json").token);
 const prefix = "m!";
 
+
 client.on("ready", () => {
     console.log("Есть пробитие!");
     client.editStatus("online", {
@@ -14,13 +15,13 @@ client.on("messageCreate", message => {
     if (!message.content.startsWith(prefix) || message.author.client) return;
     const args = message.content.slice(prefix.length).trim().split(/ +/g);
     const command = args.shift().toLowerCase();
-
+    
     switch (command) {
         case "help":
             client.createMessage(message.channel.id, {
                 embed: {
                     title: "СПИСОК КОМАНД",
-                    description: "`m!help` - список команд **(вы здесь)**\n`m!invite` - добавить бота на свой сервер\n`m!kotletki` - местный !ping\n`m!say <text>` - текст от имени бота\n\n**Список будет дополняться по мере появления новых команд.**",
+                    description: "`m!help` - список команд **(вы здесь)**\n`m!hello` - передать привет боту\n`m!invite` - добавить бота на свой сервер\n`m!kotletki` - местный !ping\n`m!say <text>` - отправить текст от имени бота\n\n**Список будет дополняться по мере появления новых команд.**",
                     author: {
                         name: "m1t3nk0v.b0t",
                         icon_url: "https://cdn.discordapp.com/attachments/496735656907636746/688425119797870762/m1t3nk0v_avatar.png"
@@ -34,8 +35,12 @@ client.on("messageCreate", message => {
             });
             break;
 
+        case "hello":
+            client.createMessage(message.channel.id, `:wave: Привет, ${message.author.mention}!`);
+            break;
+
         case "invite":
-            if(message.author.id === "425356730483081227") {
+            if (message.author.id === "425356730483081227") {
                 client.createMessage(message.channel.id, `${message.author.mention} а жопа не слипнется?)`);
             }
             else {
@@ -44,7 +49,7 @@ client.on("messageCreate", message => {
             break;
 
         case "kotletki":
-            if(message.author.id === "425356730483081227") {
+            if (message.author.id === "425356730483081227") {
                 client.createMessage(message.channel.id, `${message.author.mention} сасай, никаких тебе котлеток)`);
             }
             else {
@@ -53,10 +58,10 @@ client.on("messageCreate", message => {
             break;
 
         case "say":
-            if(args.length === 0) {
+            if (args.length === 0) {
                 client.createMessage(message.channel.id, `***(звуки молчания)***`);
             }
-            if(message.author.id === "425356730483081227") {
+            if (message.author.id === "425356730483081227") {
                 client.createMessage(message.channel.id, `${message.author.mention} иди в жопу, шутишь плохо, прикрываешься скобочками, наверное, ещё еду с пола ешь`);
             }
             else {
