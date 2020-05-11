35ecad7bfcc2574833c303b39ca0655bd3405e44
Ivan Mitenkov <39021235+MitenkovIvan@users.noreply.github.com>
2020-05-11 19:19:27 +0300

    8ball

--- index.js
+++ index.js
@@ -17,11 +17,77 @@ client.on("messageCreate", message => {
     const command = args.shift().toLowerCase();
     
     switch (command) {
+		case "8ball":
+		    const question = args.join(" ");
+			const response = [
+				`Да.`,
+				`Нет.`,
+                `Возможно.`,
+                `Может быть.`,
+                `Посмотрим.`,
+                `Поживём - увидим.`,
+                `Вряд ли.`,
+                `Не-а.`,
+                `А чё?)`,
+                `Попробуй ещё раз.`,
+                `На этот вопрос у меня нет ответа.`,
+                `Попробуй в Интернете найти, там этого полно.`,
+                `Ищи сам ответ на свой вопрос!`,
+                `НИХУЯ НЕ ПОНИМАЮ НИ ОДНОГО СЛОВА БЛЯТЬ КАРТАВАЯ СУКА`,
+                `Это что за вопросы?!`,
+                `Я на такие вопросы не отвечаю.`,
+                `Давай что-нибудь другое, я затрудняюсь ответить.`,
+                `Очень смешно)0)`,
+                `Автор, ты сука?!`,
+                `Не смешно.`,
+                `А ты КВНщик или камедиклабер?`,
+                `А зачем тебе это?`,
+                `А оно тебе надо?)`,
+                `В чём смысл этого вопроса?`,
+                `Меньше знаешь - меньше... знаешь.`,
+                `Вырастешь - узнаешь.`,
+                `Тебе ещё рано это знать)`,
+                `:flushed: Мммм... вот... нууу, кхм, пук.`,
+                `:zipper_mouth:`
+			];
+            const answer = response[Math.floor(Math.random()*response.length)];
+            if (args.length === 0) {
+                client.createMessage(message.channel.id, `${message.author.mention} сначала вопрос придумай, а уже потом спрашивай -_-`);
+            }
+            else {
+			client.createMessage(message.channel.id, {
+                embed: {
+                    author: {
+                        name: `${message.author.username}`,
+                        icon_url: `${message.author.avatarURL}`
+                    },
+                    color: 7237340,
+                    footer: {
+                        text: "© 2020 m1t3nk0v | Created with Eris.",
+                        icon_url: "https://cdn.discordapp.com/attachments/496735656907636746/688425119797870762/m1t3nk0v_avatar.png"
+                    },
+					fields: [
+                    {
+                        name: "Вопрос:",
+                        value: `${question}`,
+                    },
+                    {
+                        name: "Ответ:",
+                        value: `${answer}`,
+                    }]
+                }
+            })};
+            break;
+			
+		case "hello":
+            client.createMessage(message.channel.id, `:wave: Привет, ${message.author.mention}!`);
+            break;
+
         case "help":
             client.createMessage(message.channel.id, {
                 embed: {
                     title: "СПИСОК КОМАНД",
-                    description: "`m!help` - список команд **(вы здесь)**\n`m!hello` - передать привет боту\n`m!invite` - добавить бота на свой сервер\n`m!kotletki` - местный !ping\n`m!reverse <text>` - отправить текст наоборот от имени бота\n`m!say <text>` - отправить текст от имени бота\n\n**Список будет дополняться по мере появления новых команд.**",
+                    description: "`m!8ball <question>` - задать вопрос боту\n`m!hello` - передать привет боту\n`m!help` - список команд **(вы здесь)**\n`m!invite` - добавить бота на свой сервер\n`m!kotletki` - местный !ping\n`m!reverse <text>` - отправить текст наоборот от имени бота\n`m!say <text>` - отправить текст от имени бота\n\n**Список будет дополняться по мере появления новых команд.**",
                     author: {
                         name: "m1t3nk0v.b0t",
                         icon_url: "https://cdn.discordapp.com/attachments/496735656907636746/688425119797870762/m1t3nk0v_avatar.png"
@@ -33,10 +99,6 @@ client.on("messageCreate", message => {
                     }
                 }
             });
-            break;
-
-        case "hello":
-            client.createMessage(message.channel.id, `:wave: Привет, ${message.author.mention}!`);
             break;
 
         case "invite":
